42da395c8fbc35f6f335c64805f0b0c166869a1c
a-penton <57507806+a-penton@users.noreply.github.com>
2022-09-18 11:48:41 -0400

    Update cop3530_3_1.h
    Add destructor function and brief descriptive comments.

--- 3_Trees/sum_of_level/src/cop3530_3_1.h
+++ 3_Trees/sum_of_level/src/cop3530_3_1.h
@@ -36,6 +36,7 @@
        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  };
 
+// This will be called in main to construct the tree
 TreeNode* insert(TreeNode* root, int data) 
 {
       if (root == nullptr)
@@ -45,6 +46,13 @@ TreeNode* insert(TreeNode* root, int data)
       else 
         root->left = insert(root->left, data);
       return root;
+}
+
+// This will be called in main to deallocate memory
+void destruct(TreeNode* root) {
+	if (root->left) destruct(root->left);
+	if (root->right) destruct(root->right);
+	delete root;
 }
 
 int sumOfLevel(TreeNode* root, int level) 
