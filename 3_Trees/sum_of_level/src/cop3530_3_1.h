36673b95d54700d2dc38b01fb605fa2b7fb57d47
kapooramanpreet <kapooramanpreet@ufl.edu>
2022-09-18 11:56:41 -0400

    Spacing fixed for consistency

--- 3_Trees/sum_of_level/src/cop3530_3_1.h
+++ 3_Trees/sum_of_level/src/cop3530_3_1.h
@@ -29,29 +29,32 @@
 
  class TreeNode 
  {
-     public:
-       int val;
-       TreeNode *left;
-       TreeNode *right;
-       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    public:
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  };
 
 // This will be called in main to construct the tree
 TreeNode* insert(TreeNode* root, int data) 
 {
-      if (root == nullptr)
-        return new TreeNode(data);
-      if (root->val < data)
-        root->right = insert(root->right, data);
-      else 
-        root->left = insert(root->left, data);
-      return root;
+  if (root == nullptr)
+    return new TreeNode(data);
+  if (root->val < data)
+    root->right = insert(root->right, data);
+  else 
+    root->left = insert(root->left, data);
+  return root;
 }
 
 // This will be called in main to deallocate memory
-void destruct(TreeNode* root) {
-	if (root->left) destruct(root->left);
-	if (root->right) destruct(root->right);
+void destruct(TreeNode* root) 
+{
+	if (root->left) 
+    destruct(root->left);
+	if (root->right) 
+    destruct(root->right);
 	delete root;
 }
 
