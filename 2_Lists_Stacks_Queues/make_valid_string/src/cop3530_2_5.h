77930995291ec27cf2e2a439856862130fd42051
kapooramanpreet <kapooramanpreet@ufl.edu>
2023-03-24 16:29:57 -0400

    added text to Matthew's stack question

--- 2_Lists_Stacks_Queues/make_valid_string/src/cop3530_2_5.h
+++ 2_Lists_Stacks_Queues/make_valid_string/src/cop3530_2_5.h
@@ -1,3 +1,22 @@
+/*
+	Make Valid String
+
+	You are given a string `str` that contains only the characters 
+	`(`, `)`, `{`, `}`, `[`, `]`. The string `str` is valid if:
+
+		- each opening bracket is closed by a matching bracket
+		- each bracket is closed in the correct order
+		- each closing bracket has a corresponding opening bracket
+
+	If `str` is not valid then add the minimum number of correct 
+	brackets to make `str` valid and return the new string, else return `str`.
+
+	Sample Test:
+	Input: str = "("
+	Output: "()"
+	Explanation: The opening parenthesis, must be closed by a matching closed parenthesis
+*/
+
 #include <iostream>
 using std::string;
 
