7d0c03eab5ce46470e2a2fb67417ce5c3c7aa318
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-04-17 20:48:19 -0400

    Changed trust vector to know vector

--- 7_Graph_Algorithms/hidden_gator/src/cop3530_7_3.h
+++ 7_Graph_Algorithms/hidden_gator/src/cop3530_7_3.h
@@ -1,6 +1,6 @@
 #include <vector>
 
-int hiddenGator(int n, std::vector<std::vector<int>> trust)
+int hiddenGator(int n, std::vector<std::vector<int>> know)
 {
     // your code here :)
     return 0;
