af57ad38befb254e3497dd8206fddca5b19c6090
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-05-08 11:37:11 -0400

    Added Four-digit numbers problem

---
+++ 5_Sets_Maps/four_digit_numbers/test-unit/test.cpp
@@ -0,0 +1,22 @@
+#include "../src/cop3530_5_3.h"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+/*
+    To check output (At the four_digit_numers directory):
+        g++ -std=c++14 -Werror -Wuninitialized -o test test-unit/test.cpp && ./test
+*/
+
+TEST_CASE("Case 1")
+{
+    REQUIRE(fourDigitNumbers("0123") == std::vector<int>{1023, 1203, 2013, 2031, 2103, 2301, 3021});
+}
+
+TEST_CASE("Case 2")
+{
+    REQUIRE(fourDigitNumbers("11220") == std::vector<int>{1021, 1201, 1221, 2011, 2021, 2101, 2121, 2201, 2211});
+}
+TEST_CASE("Case 3")
+{
+    REQUIRE(fourDigitNumbers("0022") == std::vector<int>{});
+}
