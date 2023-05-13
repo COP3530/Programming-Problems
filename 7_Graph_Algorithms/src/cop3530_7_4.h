695c9bacad96b1b7ba4d108db75ca8818833a297
kapooramanpreet <kapooramanpreet@ufl.edu>
2023-05-13 09:58:02 -0400

    cleaned valid path

--- 7_Graph_Algorithms/src/cop3530_7_4.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-bool validPath(int n, std::vector<std::pair<int, int>> edges, int source, int destination){
-    //Your code here
-
-    return false;
-}
--- 7_Graph_Algorithms/valid_path/test-unit/test.cpp
+++ 7_Graph_Algorithms/valid_path/test-unit/test.cpp
@@ -1,9 +1,9 @@
-#include "../src/cop3530_7_4.h"
+#include "../src/valid_path.h"
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
 /*
-	To check output (At the valid_path directory):
+	To check output (At valid_path directory):
 		g++ -std=c++14 -Werror -Wuninitialized -o test test-unit/test.cpp && ./test
 */
 
@@ -68,9 +68,7 @@ TEST_CASE("Function: Test 5")
     int destination = 3;
     int n = 4;
 
-    std::vector<std::pair<int,int>> edges = {
-
-    };
+    std::vector<std::pair<int,int>> edges = {};
 
     REQUIRE(validPath(n, edges, source, destination) == false);
 }
