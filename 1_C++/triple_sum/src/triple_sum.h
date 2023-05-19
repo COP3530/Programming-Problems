5f11d56e1c056118eebb011ce2db0d795311cc4a
kapooramanpreet <kapooramanpreet@ufl.edu>
2023-05-19 16:27:29 -0400

    triple sum cleaned

--- 1_C++/triple_sum/src/triple_sum.h
+++ 1_C++/triple_sum/src/triple_sum.h
@@ -1,32 +1,16 @@
 /*
     Triple Sum
 
-    Suppose you are given an array of integers nums.
-    Return all the triplets such that nums[i] + nums[j] + nums[k] == 0.
-
-    The solution must not contain duplicate triplets.
-
-    Definition: A duplicate triplet is a triplet with the exact same numbers regardless of ordering.
-
-    Sample Input:
-        nums = [-1, 0, 1, 2, -3, 2]
-
-    Sample Output:
-        [{-1, 0, 1}, {1, 2, -3}]
-
-    Explanation:
-    nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0
-    nums[2] + nums[3] + nums[4] = 1 + 2 + (-3) = 0
-
-    There are two distinct triplets: {-1, 0, 1} and {1, 2, -3}.
+    Suppose you are given an array of integers nums. Return all the 
+    triplets such that nums[i] + nums[j] + nums[k] == 0. The solution 
+    must not contain duplicate triplets.
 */
 
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-vector<vector<int>> threeSum(vector<vector<int>>& nums){
+vector<vector<int>> threeSum(vector<int>& nums){
 
     return vector<vector<int>>();
 }
--- 1_C++/triple_sum/test-unit/test.cpp
+++ 1_C++/triple_sum/test-unit/test.cpp
@@ -9,61 +9,36 @@
 
 TEST_CASE("Function: Test 1")
 {
-
-    std::vector<std::vector<int>> nums = {
-            {-1, 0, 1, 2, -3, 2}
-    };
-
+    std::vector<int> nums = {-1, 0, 1, 2, -3, 2};
     std::vector<std::vector<int>> expected = {{-1, 0, 1}, {1, 2, -3}};
-
     REQUIRE(threeSum(nums) == expected);
 }
 
 TEST_CASE("Function: Test 2")
 {
-
-    std::vector<std::vector<int>> nums = {
-            {0, 3, 1}
-    };
-
+    std::vector<int> nums = {0, 3, 1};
     std::vector<std::vector<int>> expected = {};
-
     REQUIRE(threeSum(nums) == expected);
 }
 
 TEST_CASE("Function: Test 3")
 {
-
-    std::vector<std::vector<int>> nums = {
-            {0, 0, 0}
-    };
-
+    std::vector<int> nums = {0, 0, 0};
     std::vector<std::vector<int>> expected = {{0, 0, 0}};
-
     REQUIRE(threeSum(nums) == expected);
 }
 
 TEST_CASE("Function: Test 4")
 {
-
-    std::vector<std::vector<int>> nums = {
-            {-1, -1, 2, -1, -1, 2}
-    };
-
+    std::vector<int> nums = {-1, -1, 2, -1, -1, 2};
     std::vector<std::vector<int>> expected = {{-1, -1, 2}};
-
     REQUIRE(threeSum(nums) == expected);
 }
 
 TEST_CASE("Function: Test 5")
 {
-
-    std::vector<std::vector<int>> nums = {
-            {1, 1, 3, 5, 2, 1}
-    };
-
+    std::vector<int> nums = {1, 1, 3, 5, 2, 1};
     std::vector<std::vector<int>> expected = {};
-
     REQUIRE(threeSum(nums) == expected);
 }
 
