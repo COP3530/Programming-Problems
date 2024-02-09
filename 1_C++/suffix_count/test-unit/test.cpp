f07a5e52f9197d46bf9c0dbfa6fc8bc9b229f23e
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2024-02-09 11:58:36 -0500

    Add makefile to suffix count

--- 1_C++/suffix_count/test-unit/test.cpp
+++ 1_C++/suffix_count/test-unit/test.cpp
@@ -2,22 +2,28 @@
 #include "catch/catch_amalgamated.hpp"
 
 /*
-	To check output (At the suffix_count directory):
+    To check output (At the suffix_count directory):
 
-# run this once to build catch
-g++ -std=c++14 -Werror -Wuninitialized -g -c test-unit/catch/catch_amalgamated.cpp -o build/catch_amalgamated.o
+    # If you have make...
+    # run this command
+    make
+    # If you installed gcc via the msys2 ucrt64 toolchain, you may need to run this command instead
+    mingw32-make
 
-# run this every time your tests change
-g++ -std=c++14 -Werror -Wuninitialized -g build/catch_amalgamated.o test-unit/test.cpp -o build/test && ./build/test
+    # If you do not have make...
+    # run this once to build catch
+    g++ -std=c++14 -Werror -Wuninitialized -g -c test-unit/catch/catch_amalgamated.cpp -o build/catch_amalgamated.o
 
+    # then run this command to compile and run the tests (you do not need to run the previous command again)
+    g++ -std=c++14 -Werror -Wuninitialized -g build/catch_amalgamated.o test-unit/test.cpp -o build/test; ./build/test
 */
 
 TEST_CASE("Function: suffixCount 1", "[given]") {
-	REQUIRE(suffixCount("et tu, brute", 1) == 0);
+    REQUIRE(suffixCount("et tu, brute", 1) == 2);
 }
 
 TEST_CASE("Function: suffixCount 2", "[given]") {
-	REQUIRE(suffixCount("aaa", 1) == 3);
+    REQUIRE(suffixCount("aaa", 1) == 3);
 }
 
 // TEST_CASE("Function: suffixCount 4", "[o_hidden]") {
